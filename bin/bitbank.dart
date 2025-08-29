eff93807fc4aa3c2e775945629f53727671055ef
normidar <normidar7@gmail.com>
2025-08-29 16:01:13 +0900

    Refactor main function in bitbank.dart to streamline argument handling and improve ticker functionality

--- bin/bitbank.dart
+++ bin/bitbank.dart
@@ -10,83 +10,98 @@ void main(List<String> args) async {
     apiSecret: Platform.environment['BITBANK_SECRET']!,
   );
 
-  if (args.isEmpty) {
-    while (true) {
-      await Future<void>.delayed(const Duration(seconds: 1));
-      final price = await Bitbank.ticker(coinType: CoinType.pol);
-      final sellPrice = price.data.sell;
-      final buyPrice = price.data.buy;
-      final diff = double.parse(sellPrice) - double.parse(buyPrice);
-      final diffRate = diff / double.parse(buyPrice) * 100;
-      final volume = price.data.vol;
-      final lastPrice = price.data.last;
-      final volYen = double.parse(volume) * double.parse(lastPrice);
-      final highPrice = price.data.high;
-      final lowPrice = price.data.low;
-      final timestamp = price.data.timestamp;
-      print('--------------------------------');
-      print('sellPrice: $sellPrice');
-      print('buyPrice: $buyPrice');
-      print('diff: ${diff.toStringAsFixed(3)}');
-      print('diffRate: ${diffRate.toStringAsFixed(3)}%');
-      print('volume: $volume');
-      print('lastPrice: $lastPrice');
-      print('volYen: ${int.parse(volYen.toStringAsFixed(0))}');
-      print('highPrice: $highPrice');
-      print('lowPrice: $lowPrice');
-      print('${DateTime.fromMillisecondsSinceEpoch(timestamp)}');
-    }
-  } else {
-    switch (args.length) {
-      case 1:
-        final param = args.first;
-        if (param == 'assets') {
-          final assets = await bitbank.assets();
-          for (final asset in assets.data.assets) {
-            if (double.parse(asset.onhandAmount) > 0) {
-              print('${asset.asset}: ${asset.onhandAmount}');
-            }
+  switch (args.length) {
+    case 1:
+      final param = args.first;
+      if (param == 'assets') {
+        final assets = await bitbank.assets();
+        for (final asset in assets.data.assets) {
+          if (double.parse(asset.onhandAmount) > 0) {
+            print('${asset.asset}: ${asset.onhandAmount}');
           }
-          return;
         }
-        final coinType = args.first;
-        final price = await Bitbank.ticker(
-          coinType: CoinType.values.firstWhere((e) => e.name == coinType),
-        );
-        print(price);
-      case 4: // sell pol 1000000 37.612
-        assert(args[0] == 'buy' || args[0] == 'sell', 'Invalid command');
-        final command = args[0];
-        final coinType = CoinType.values.firstWhere((e) => e.name == args[1]);
-        final amountYen = args[2];
-        final price = args[3];
-        final amount = (double.parse(amountYen) / double.parse(price))
-            .toStringAsFixed(4);
+        return;
+      }
 
-        final order = await bitbank.createOrder(
-          coinType: coinType,
-          side: command,
-          type: 'limit',
-          amount: amount,
-          postOnly: true,
-          price: price,
-        );
-        while (true) {
-          await Future<void>.delayed(const Duration(seconds: 1));
-          final orderStatus = await bitbank.getOrder(
-            coinType: coinType,
-            orderId: order.data.orderId,
+      // ticker
+      final coinType = CoinType.values.firstWhere((e) => e.name == param);
+      while (true) {
+        await Future<void>.delayed(const Duration(seconds: 1));
+        final price = await Bitbank.ticker(coinType: coinType);
+        final sellPrice = price.data.sell;
+        final buyPrice = price.data.buy;
+        final diff = double.parse(sellPrice) - double.parse(buyPrice);
+        final diffRate = diff / double.parse(buyPrice) * 100;
+        final volume = price.data.vol;
+        final lastPrice = price.data.last;
+        final volYen = double.parse(volume) * double.parse(lastPrice);
+        final highPrice = price.data.high;
+        final lowPrice = price.data.low;
+        final timestamp = price.data.timestamp;
+        print('--------------------------------');
+        print('sellPrice: $sellPrice');
+        print('buyPrice: $buyPrice');
+        print('diff: ${diff.toStringAsFixed(3)}');
+        print('diffRate: ${diffRate.toStringAsFixed(3)}%');
+        print('volume: $volume');
+        print('lastPrice: $lastPrice');
+        print('volYen: ${int.parse(volYen.toStringAsFixed(0))}');
+        print('highPrice: $highPrice');
+        print('lowPrice: $lowPrice');
+        print('${DateTime.fromMillisecondsSinceEpoch(timestamp)}');
+      }
+    case 2:
+      final param = args.first;
+      if (param == 'orders') {
+        final coinType = CoinType.values.firstWhere((e) => e.name == args[1]);
+        final orders = await bitbank.getActiveOrders(coinType: coinType);
+        for (final order in orders) {
+          final executedPrice =
+              (double.parse(order.price) * double.parse(order.executedAmount))
+                  .toStringAsFixed(0);
+          final startPrice =
+              (double.parse(order.price) * double.parse(order.startAmount))
+                  .toStringAsFixed(0);
+          print(
+            '${order.orderId} ${order.price}¥ '
+            '${order.side} amount:${order.executedAmount}/${order.startAmount} '
+            'price:$executedPrice¥/$startPrice¥',
           );
-          print('------------------------');
-          print('orderId: ${orderStatus.data.orderId}');
-          print('price: ${orderStatus.data.price}');
-          print('status: ${orderStatus.data.status}');
-          print('startAmount: ${orderStatus.data.startAmount}');
-          print('executedAmount: ${orderStatus.data.executedAmount}');
-          print('remainingAmount: ${orderStatus.data.remainingAmount}');
         }
-      default:
-        print('Invalid arguments');
-    }
+        return;
+      }
+    case 4: // sell pol 1000000 37.612
+      assert(args[0] == 'buy' || args[0] == 'sell', 'Invalid command');
+      final command = args[0];
+      final coinType = CoinType.values.firstWhere((e) => e.name == args[1]);
+      final amountYen = args[2];
+      final price = args[3];
+      final amount = (double.parse(amountYen) / double.parse(price))
+          .toStringAsFixed(4);
+
+      final order = await bitbank.createOrder(
+        coinType: coinType,
+        side: command,
+        type: 'limit',
+        amount: amount,
+        postOnly: true,
+        price: price,
+      );
+      while (true) {
+        await Future<void>.delayed(const Duration(seconds: 1));
+        final orderStatus = await bitbank.getOrder(
+          coinType: coinType,
+          orderId: order.data.orderId,
+        );
+        print('------------------------');
+        print('orderId: ${orderStatus.data.orderId}');
+        print('price: ${orderStatus.data.price}');
+        print('status: ${orderStatus.data.status}');
+        print('startAmount: ${orderStatus.data.startAmount}');
+        print('executedAmount: ${orderStatus.data.executedAmount}');
+        print('remainingAmount: ${orderStatus.data.remainingAmount}');
+      }
+    default:
+      print('Invalid arguments');
   }
 }
